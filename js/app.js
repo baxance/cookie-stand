f2de90e2071eadc80eceebc1434c30f9a3c4391d
Barrett <baxance@protonmail.com>
2021-01-18 23:28:46 -0800

    lab6 finished I guess. didn't do headers for lists in JS

--- js/app.js
+++ js/app.js
@@ -1,66 +1,162 @@
 'use strict'
 
+let cookHours = ['6am: ','7am: ','8am:','9am:','10am:','11am:','12pm:','1pm:','2pm: ','3pm: ','4pm: ','5pm: ','6pm: ','7pm: ']
+
 let seattle = { // 23/65 min/max 6.3 avg
-  name: 'Salmon Cookies',
+  name: 'Seattle',
   minCust: 23,
   maxCust: 65,
   avgCust: 6.3,
-  hourly: [6,7,8,9,10,11,12,1,2,3,4,5,6,7,8],
-  genHourly: function () {
-    let randomCust = Math.floor((Math.random() * (this.maxCust - this.minCust)) + this.minCust);
-    // this.hourly = randomCust;
-    return randomCust;
-  }
-}
-let array = [];
-for (let i = 0; i < seattle.hourly.length; i++) {
-  array.push(seattle.genHourly())
-}
+  cookSales: [],
+  totalSales: 0,
+  sales: function () {
+    let salesList = document.getElementById('salesListSea');
+    for (let i = 0; i < cookHours.length; i++) {
+      let salesPerHour = Math.random() * (this.maxCust - this.minCust) + this.minCust;
+      salesPerHour = Math.floor(salesPerHour * this.avgCust / cookHours.length);
 
-console.log(array);
+      let column = document.createElement('li');
+      column.textContent = cookHours[i] + salesPerHour + ' cookies';
 
-// var divElement = document.createElement('div');
-// var h2Element = document.createElement('h2'); 
-// h2Element.textContent = seattle.name;
-// divElement.appendChild(h2Element);
-// var contentId = document.getElementById('content');
-// contentId.appendChild(divElement);
-
-
-function createTree(list, hourly) {
-  let ul = list.appendChild(document.createElement('ul'));
-  for (let [key, val] of Object.entries(hourly)) {
-    let li = ul.appendChild(document.createElement('li'));
-    li.textContent = key;
-    if (Object.keys(val).length) {
-      createTree(li, val);
+      salesList.appendChild(column);
+      this.totalSales = this.totalSales + salesPerHour;
+      this.cookSales.push(this.totalSales);
+    
     }
+    
+    let columnTotal = document.createElement('li');
+    columnTotal.textContent = 'Total: ' + this.totalSales + ' cookies',
+    salesList.appendChild(columnTotal);
+
   }
+};
+
+seattle.sales();
+
+/////////////////ctrl + C / ctrl + V Time!//////////////////////////////////////////
+
+let tokyo = { // 3/24 min/max 1.2 avg
+    name: 'Tokyo',
+    minCust: 3,
+    maxCust: 24,
+    avgCust: 1.2,
+    cookSales: [],
+    totalSales: 0,
+    sales: function () {
+      let salesList = document.getElementById('salesListTokyo');
+      for (let i = 0; i < cookHours.length; i++) {
+        let salesPerHour = Math.random() * (this.maxCust - this.minCust) + this.minCust;
+        salesPerHour = Math.floor(salesPerHour * this.avgCust / cookHours.length);
+  
+        let column = document.createElement('li');
+        column.textContent = cookHours[i] + salesPerHour + ' cookies';
+  
+        salesList.appendChild(column);
+        this.totalSales = this.totalSales + salesPerHour;
+        this.cookSales.push(this.totalSales);
+      
+      }
+      
+      let columnTotal = document.createElement('li');
+      columnTotal.textContent = 'Total: ' + this.totalSales + ' cookies',
+      salesList.appendChild(columnTotal);
+  
+    }
+  };
+
+tokyo.sales();
+
+
+let dubai = { // 11/38 min/max 3.7 avg
+  name: 'Dubai',
+  minCust: 11,
+  maxCust: 38,
+  avgCust: 3.7,
+  cookSales: [],
+  totalSales: 0,
+  sales: function () {
+    let salesList = document.getElementById('salesListDub');
+    for (let i = 0; i < cookHours.length; i++) {
+      let salesPerHour = Math.random() * (this.maxCust - this.minCust) + this.minCust;
+      salesPerHour = Math.floor(salesPerHour * this.avgCust / cookHours.length);
+
+      let column = document.createElement('li');
+      column.textContent = cookHours[i] + salesPerHour + ' cookies';
+
+      salesList.appendChild(column);
+      this.totalSales = this.totalSales + salesPerHour;
+      this.cookSales.push(this.totalSales);
+    
+    }
+    
+    let columnTotal = document.createElement('li');
+    columnTotal.textContent = 'Total: ' + this.totalSales + ' cookies',
+    salesList.appendChild(columnTotal);
+
+  }
+};
+
+dubai.sales();
+
+let paris = { // 20/38 min/max 2.3 avg
+  name: 'Paris',
+  minCust: 20,
+  maxCust: 38,
+  avgCust: 2.3,
+  cookSales: [],
+  totalSales: 0,
+  sales: function () {
+    let salesList = document.getElementById('salesListParis');
+    for (let i = 0; i < cookHours.length; i++) {
+      let salesPerHour = Math.random() * (this.maxCust - this.minCust) + this.minCust;
+      salesPerHour = Math.floor(salesPerHour * this.avgCust / cookHours.length);
+
+      let column = document.createElement('li');
+      column.textContent = cookHours[i] + salesPerHour + ' cookies';
+
+      salesList.appendChild(column);
+      this.totalSales = this.totalSales + salesPerHour;
+      this.cookSales.push(this.totalSales);
+    
+    }
+    
+    let columnTotal = document.createElement('li');
+    columnTotal.textContent = 'Total: ' + this.totalSales + ' cookies',
+    salesList.appendChild(columnTotal);
+
+  }
+};
+
+paris.sales();
+
+
+let lima = { // 2/16 min/max 4.6 avg
+name: 'Lima',
+minCust: 20,
+maxCust: 38,
+avgCust: 2.3,
+cookSales: [],
+totalSales: 0,
+sales: function () {
+  let salesList = document.getElementById('salesListLima');
+  for (let i = 0; i < cookHours.length; i++) {
+    let salesPerHour = Math.random() * (this.maxCust - this.minCust) + this.minCust;
+    salesPerHour = Math.floor(salesPerHour * this.avgCust / cookHours.length);
+
+    let column = document.createElement('li');
+    column.textContent = cookHours[i] + salesPerHour + ' cookies';
+
+    salesList.appendChild(column);
+    this.totalSales = this.totalSales + salesPerHour;
+    this.cookSales.push(this.totalSales);
+  
+  }
+  
+  let columnTotal = document.createElement('li');
+  columnTotal.textContent = 'Total: ' + this.totalSales + ' cookies',
+  salesList.appendChild(columnTotal);
+
 }
+};
 
-createTree(document.getElementById('container'), seattle.hourly);
-
-// ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'] hours as string
-// [6,7,8,9,10,11,12,1,2,3,4,5,6,7,8]  hours as number
-// for (let i = 0; i < hourly.length; i++) {
-//  seattle.genHourly()
-
-// }
-
-// seattle.genHourly()
-
-// let tokyo = { // 3/24 min/max 1.2 avg
-
-// }
-
-// let dubai = { // 11/38 min/max 3.7 avg
-
-// }
-
-// let paris = { // 20/38 min/max 2.3 avg
-
-// }
-
-// let lima = { // 2/16 min/max 4.6 avg
-
-// }
+lima.sales();
